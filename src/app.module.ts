e62e3b2310913749d0586e6a59493ba800dca7d0
oitediresacajamarca <oite.diresa.cajamarca@gmail.com>
2021-09-22 12:13:04 -0500

    .se agregara conexiones a multiples bases de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,22 +10,44 @@ import { AppService } from './app.service';
 
 import { DistribucionVacunasModule } from './distribucion-vacunas/distribucion-vacunas.module';
 
+const defaultOptions = {
+  type: 'mssql',
+  host: 'localhost',
+  port: 1433,
+  username: 'sa',
+  synchronize: false,
+};
+
 
 @Module({
   imports: [
- 
-    
     TypeOrmModule.forRoot({
       type: 'mssql',
       host: 'localhost',
       port: 1433,
       username: 'sa',
+      synchronize: false,
       password: '.',
-      database: 'BD_VACUNACION_COVID',     
+      database: 'BD_VACUNACION_COVID',
       entities: [
         "dist/**/*.entity{.ts,.js}"
       ],
+      "extra": {
+        "validateConnection": false,
+        "trustServerCertificate": true
+      }
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: 'localhost',
+      port: 1433,
+      username: 'sa',
       synchronize: false,
+      password: '.',
+      database: 'risc_2030',
+      entities: [
+        "dist/**/*.entity{.ts,.js}"
+      ],
       "extra": {
         "validateConnection": false,
         "trustServerCertificate": true
@@ -35,10 +57,10 @@ import { DistribucionVacunasModule } from './distribucion-vacunas/distribucion-v
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'tablero_vacunacion'),
     })
-    
-   
+
+
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
