0fcf214fde3590a88d5f30a7b60a13eb49c3a2a4
oitediresacajamarca <oite.diresa.cajamarca@gmail.com>
2022-05-24 09:55:49 -0500

    afiliados sis

--- src/bases-externas/afiliados-sis/afiliados-sis.controller.ts
+++ src/bases-externas/afiliados-sis/afiliados-sis.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PadronReniecEntity } from 'src/distribucion-vacunas/padron-reniec/padron-reniec.entity';
 import { PadronReniec } from 'src/distribucion-vacunas/padron-reniec/padron-reniec.interface';
@@ -8,62 +8,59 @@ import { RenaesEntity } from '../renaes.entity';
 
 @Controller('afiliados-sis')
 export class AfiliadosSisController {
-    constructor(@InjectRepository(AfiliadosSisEntity, 'AFILIADOS_SIS')
-    private afiliadosrep: Repository<AfiliadosSisEntity> ,
+  constructor(@InjectRepository(AfiliadosSisEntity, 'AFILIADOS_SIS')
+  private afiliadosrep: Repository<AfiliadosSisEntity>,
     @InjectRepository(RenaesEntity, 'AFILIADOS_SIS')
     private renaesrep: Repository<RenaesEntity>,
     @InjectRepository(PadronReniecEntity, 'default')
     private padronrep: Repository<PadronReniecEntity>) {
 
 
-    }
+  }
 
-    @Get('1')
-    async devolver_adiliados() {
-       let resp= await this.afiliadosrep.find({where:{afi_apmaterno:'OCAS',afi_appaterno:'MIRANDA'}})
-       return resp;
-    }
-    @Get('renaes')
-    async devolver_renaes() {
-       let resp= await this.renaesrep.find()
-       return resp;
-    }
-    @Get('padron')
-    async devolver_padron() {
-       let resp= await this.padronrep.find({where:{Numero_Doc:'41935041'}})
-       return resp;
-    }
+  @Get('1')
+  async devolver_adiliados() {
+    let resp = await this.afiliadosrep.find({ where: { afi_apmaterno: 'OCAS', afi_appaterno: 'MIRANDA' } })
+    return resp;
+  }
+  @Get('renaes')
+  async devolver_renaes() {
+    let resp = await this.renaesrep.find()
+    return resp;
+  }
+  @Get('padrones/:renipress')
+  async devolver_padron(@Param('renipress') renipress: string) {
+    console.log(renipress)
+    let resp = await this.padronrep.find({ where: { Renipress_Padron: renipress } })
+    return resp;
+  }
 
-    @Get('actualizar')
-    async actualizar_renipress_padron() {
-        let resp: PadronReniec[] = await this.padronrep.find({Edad:41 })
-let count=0
-  resp.forEach(async res => {
-      try{
-         /*    this.afiliadosrep.findOne({ where: { afi_DNI: res.Numero_Doc } }).then(async (enc) => {
-                if(enc!=undefined){
-                  res.Renipress_Padron=enc.afi_idEESSAdscripcion
-                  await  this.padronrep.save(res);
-                }else{
-                    console.log(count)
-                
-                }
-          
-            }).catch((err) => {})*/
-            count=count+1
+  @Get('actualizar')
+  async actualizar_renipress_padron() {
+    let resp: PadronReniec[] = await this.padronrep.find({ where: { Edad: 41 } })
+    let count = 0
+    resp.forEach(async res => {
+      try {
+        this.afiliadosrep.findOne({ where: { afi_DNI: res.Numero_Doc } }).then(async (enc) => {
+          if (enc != undefined) {
+            res.Renipress_Padron = enc.afi_idEESSAdscripcion
+            await this.padronrep.save({}, { data: res });
+          } else {
             console.log(count)
-          //  res.Renipress_Padron=enc.afi_idEESSAdscripcion
-         
-           
-          
-        }catch (e){
-          
-        }
-    //      await  this.padronrep.save(res);
-       
-        })
+          }
 
-        console.log('termino')
-    }
+        }).catch((err) => { })
+        count = count + 1
+        console.log(count)
+        //  res.Renipress_Padron=enc.afi_idEESSAdscripcion
+      } catch (e) {
+
+      }
+      //      await  this.padronrep.save(res);
+
+    })
+
+    console.log('termino')
+  }
 
 }
